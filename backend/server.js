cf0ce89d19b4a3ca1e2a19a8d024f3e0b9cb5d40
1Ansh1 <ansh.upadhyay4201@gmail.com>
2025-12-29 22:38:01 +0530

    Fix case-sensitive model import for production

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const Feedback = require("./models/feedback");
+const Feedback = require("./models/Feedback");
 
 const app = express();
 app.use(express.json());
