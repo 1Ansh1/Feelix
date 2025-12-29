985c5f704e23878ca1cf787e36243327ed7a5e08
1Ansh1 <ansh.upadhyay4201@gmail.com>
2025-12-30 00:48:22 +0530

    Fix LightPillar.css import case sensitivity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import theme from "./theme";
-import "./reactbits/lightPillar.css";
+import "./reactbits/LightPillar.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
